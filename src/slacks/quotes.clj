64467de40d8ffee1b502819a5f30917317f43d8c
Peter Fraenkel <pnf@podsnap.com>
2014-08-11 17:56:16 -0400

    Fiddle with printout

--- src/slacks/quotes.clj
+++ src/slacks/quotes.clj
@@ -1,20 +1,16 @@
 (ns slacks.quotes)
 
 
-(def quotes (atom nil))
-(def token (atom nil))
-
+(def ^{:private true} quotes (atom nil))
+(def ^{:private true} token (atom nil))
 (defn init [] 
   (let [y   (slurp "LINES")
         ys  (clojure.string/split y #"\000")
         ys  (drop 1 ys)
         t   (slurp "TOKEN")]
-    (println "Read" (count ys) "lines and token" t)
+    (println "Read" (count ys) "lines and token" @token)
     (reset! quotes ys)
     (reset! token t)))
-
-
 (defn get-quote [] (rand-nth @quotes))
-
 (defn request-ok? [params] (= (:token params) @token))
 
